aa3412463641656a82dd0d72443736b44d7a77f4
lzh <2115883273@qq.com>
2024-04-15 22:47:51 +0800

    手脚架增加vlifator

---
+++ models/params.go
@@ -0,0 +1,8 @@
+package models
+
+type UserParams struct {
+	Username   string `json:"username" binding:"required"`
+	Password   string `json:"password" binding:"required"`
+	RePassword string `json:"rePassword" binding:"required,eqfield=Password"`
+	Email      string `json:"email" binding:"required"`
+}
