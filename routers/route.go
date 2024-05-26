33147ac7a5d8ad7c4d428044ec942c8b1678de07
lzh <2115883273@qq.com>
2024-05-26 14:22:18 +0800

    添加下载功能

--- routers/route.go
+++ routers/route.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"path/filepath"
 )
 
 var r *gin.Engine
@@ -21,8 +22,20 @@ func SetupRouter(mode string) *gin.Engine {
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
 	r.Use(cors.New(config))
-
+	//静态文件
 	r.Static("/img", "/app/statics/img")
+
+	//下载功能
+	r.GET("/download/:format/:filename", func(c *gin.Context) {
+		format := c.Param("format")
+		filename := c.Param("filename")
+		filePath := filepath.Join("../scaffold-statics", format, filename)
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Transfer-Encoding", "binary")
+		c.Header("Content-Disposition", "attachment; filename="+filename)
+		c.Header("Content-Type", "application/octet-stream")
+		c.File(filePath)
+	})
 
 	r.NoRoute(func(c *gin.Context) {
 		c.JSONP(http.StatusNotFound, gin.H{
